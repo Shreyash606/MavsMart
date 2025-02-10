c491b6568efa2310c9222a16011f9492eb8a03b4
Shreyash Thakre <shreyashthakrey606@gmail.com>
2025-02-10 11:10:16 -0600

    Changes

--- mavsmart-frontend/src/components/MainHeader.js
+++ mavsmart-frontend/src/components/MainHeader.js
@@ -180,7 +180,7 @@ const MainHeader = () => {
           ) : (
             <button
               type="button"
-              className="text-white bg-[#0064b1] hover:bg-slate-500 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2 text-center inline-flex items-center me-2 lg:px-5 lg:py-2.5"
+              className="text-white bg-[#0064b1] hover:bg-slate-500 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2 text-center inline-flex items-center me-2 sm:px-5 sm:py-2.5"
             >
               <Link to="/login">Login</Link>
             </button>
